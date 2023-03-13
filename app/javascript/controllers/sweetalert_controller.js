13167405e2a987af6357c6d6a67e266367c202fb
Nicolas BRIEN <brien.nicolas@gmail.com>
2023-03-13 17:02:45 +0100

    update

--- app/javascript/controllers/sweetalert_controller.js
+++ app/javascript/controllers/sweetalert_controller.js
@@ -1,7 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 import Swal from 'sweetalert2'
 
-// Connects to data-controller="sweetalert"
 export default class extends Controller {
   connect() {
 
@@ -43,5 +42,27 @@ export default class extends Controller {
       icon: 'success',
       title: 'You just found your next co-founder'
     })
+  }
+
+  upload(){
+    const { value: file } = await Swal.fire({
+      title: 'Select image',
+      input: 'file',
+      inputAttributes: {
+        'accept': 'image/*',
+        'aria-label': 'Upload your profile picture'
+      }
+    })
+    if (file) {
+      const reader = new FileReader()
+      reader.onload = (e) => {
+        Swal.fire({
+          title: 'Your uploaded picture',
+          imageUrl: e.target.result,
+          imageAlt: 'The uploaded picture'
+        })
+      }
+      reader.readAsDataURL(file)
+    }
   }
 }
