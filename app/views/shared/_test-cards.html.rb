f6d7c7b750aa555d02b411d4168f94cbb04ce687
Justine Carles <justineacm@gmail.com>
2023-03-14 19:47:06 +0100

    rework carousel

---
+++ app/views/shared/_test-cards.html.rb
@@ -0,0 +1,21 @@
+<div class="container">
+  <ul class="cards">
+    <li class="card">
+      <div>
+        <h3 class="card-title">Service 1</h3>
+        <div class="card-content">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+      </div>
+      <div class="card-link-wrapper">
+        <a href="" class="card-link">Learn More</a>
+      </div>
+    </li>
+  </ul>
+</div>
+<footer class="page-footer">
+  <span>made by </span>
+  <a href="https://georgemartsoukos.com/" target="_blank">
+    <img width="24" height="24" src="https://assets.codepen.io/162656/george-martsoukos-small-logo.svg" alt="George Martsoukos logo">
+  </a>
+</footer>
